32bd5b4301076e93156d963953d0386d6cd0b7df
Milton Franco Ruiz <miltonfruizok@outlook.es>
2025-02-13 04:30:32 -0300

    Actualización 13/02: Modificación archivo app.py

--- app.py
+++ app.py
@@ -1,13 +1,26 @@
 from flask import Flask
+from flask_login import LoginManager
 from config import Config
-from models import db
+from models import db, bcrypt, User
 from routes import main
+from auth_routes import auth
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 db.init_app(app)
+bcrypt.init_app(app)
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'auth.login'
+login_manager.login_message_category = 'warning'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+app.register_blueprint(auth, url_prefix='/auth')
 app.register_blueprint(main)
 
 with app.app_context():
